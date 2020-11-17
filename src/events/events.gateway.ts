295ddcc95f760a0934e3be08320838399b6d1c5f
ljs <lenon461@gmail.com>
2020-11-17 16:05:17 +0900

    Websocket event set

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,4 +1,9 @@
 import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
@@ -8,19 +13,35 @@ import { Server } from 'ws';
 import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { GatewayMetadata } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { setInterval } from 'timers';
+import * as moment from "moment";
+import { Socket } from 'net';
 
 @WebSocketGateway(8090)
-export class EventsGateway {
-  @WebSocketServer()
-  server: Server;
+export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  
+  @WebSocketServer() server: Server;
 
-  @SubscribeMessage('')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  public afterInit(server: Server) {
+    console.log('   Socket is Running on \n\t8090 port');
+  }
+  
+  public handleConnection(client: any, ...args: any[]) {
+    console.log(`Client connected: ${client.id}`);
   }
 
-  @SubscribeMessage('@test')
-  onEvent(client: any, data: any): Observable<WsResponse<number>> {
-    return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
+  public handleDisconnect(client: any) {
+    console.log(`Client disconnected: ${client.id}`);
   }
+  
+  @SubscribeMessage(`Faker@kline`)
+  onEvent(
+    client, data
+  ): WsResponse<any>  {
+    const {count} =  data
+    const bar = { "time": moment().add(count || 0, 'days').format('YYYY-MM-DD'), "value": 108 }
+    return {event: "Faker@kline", data: bar}
+  }
+
 }
