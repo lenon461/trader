71fe2c270ac01d07d44fd2d4492fd7acdde0aa87
ljs <lenon461@gmail.com>
2020-11-26 18:55:22 +0900

    add order dir

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ChartModule } from './chart/chart.module';
 import { EventsModule } from './events/events.module';
 import { SummonersModule } from './summoners/summoners.module';
 import { PointsModule } from './points/points.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [ChartModule, EventsModule, SummonersModule, PointsModule],
+  imports: [ChartModule, EventsModule, SummonersModule, PointsModule, OrdersModule],
 })
 
 export class AppModule { }
